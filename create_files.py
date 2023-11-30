e05062cb249244c47ad744f7a7528bbc6a5f79ab
Sondre <31799749+sonlar@users.noreply.github.com>
2023-11-30 16:11:30 +0100

    Create script to make folder + files for each day

---
+++ create_files.py
@@ -0,0 +1,9 @@
+import os
+
+year = "2023"
+
+os.mkdir(year)
+dir = os.path.join(year + "/", "day")
+for x in range(1, 25):
+    with open(f"{dir}{x:02d}.py", "w", encoding="utf-8") as f:
+        pass
