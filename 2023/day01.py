7d3503dad4ad31788e9eb2115eb4c64355dff777
Sondre <31799749+sonlar@users.noreply.github.com>
2023-12-02 00:17:17 +0100

    Add part two solution for day01

--- 2023/day01.py
+++ 2023/day01.py
@@ -1,10 +1,27 @@
+digits = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+convert_digits = {
+    "one": "o1e",
+    "two": "t2o",
+    "three": "t3ree",
+    "four": "f4ur",
+    "five": "f5ve",
+    "six": "s6x",
+    "seven": "s7ven",
+    "eight": "e8ght",
+    "nine": "n9ne",
+}
+num = list()
+temp_num = list()
+
 with open("input.txt", "r") as f:
-    num = list()
-    tempnum = list()
     for line in f:
-        for char in line:
-            if char.isdigit():
-                tempnum.append(char)
-        num.append(int(tempnum[0] + tempnum[-1]))
-        tempnum.clear()
+        convert_line = line.strip()
+        for digit in digits:
+            if digit in line:
+                convert_line = convert_line.replace(digit, convert_digits.get(digit))
+        for char in convert_line:
+            if char.isnumeric():
+                temp_num.append(char)
+        num.append(int(temp_num[0] + temp_num[-1]))
+        temp_num.clear()
 print(sum(num))
