554ad799593f5532f327e92a1a816327482a8f98
Sondre <31799749+sonlar@users.noreply.github.com>
2023-12-02 10:40:41 +0100

    Edit script to also create day25.py

--- create_files.py
+++ create_files.py
@@ -4,6 +4,6 @@ year = "2023"
 
 os.mkdir(year)
 dir = os.path.join(year + "/", "day")
-for x in range(1, 25):
+for x in range(1, 26):
     with open(f"{dir}{x:02d}.py", "w", encoding="utf-8") as f:
         pass
