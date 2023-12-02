9820734337310e8bca9f38d10dd6e7cbe9f09bf8
Sondre <31799749+sonlar@users.noreply.github.com>
2023-12-02 21:39:41 +0100

    Add part two solution for day02

--- 2023/day02.py
+++ 2023/day02.py
@@ -1,19 +1,25 @@
-colors = {"red": 12, "green": 13, "blue": 14}
+colors = {"red": 1, "green": 1, "blue": 1}
 games = list()
-wrong = False
+power = 1
+value = 1
+
+
 with open("input.txt", "r") as f:
     for line in f:
-        line = line.strip()
-        wrong = False
-        for color, value in colors.items():
-            if wrong is True:
-                break
+        for color in colors:
+            value = 1
             for amount in range(line.count(color)):
                 if value < int(line[: line.find(color)].split()[-1]):
-                    wrong = True
-                    break
+                    colors.update({color: int(line[: line.find(color)].split()[-1])})
+                    value = int(line[: line.find(color)].split()[-1])
                 line = line.replace(color, "x", 1)
-        if wrong is False:
-            games.append(int(line[line.find(" ") + 1 : line.find(":")]))
+
+        for color, value in colors.items():
+            power *= value
+
+        games.append(power)
+        power = 1
+        colors = {"red": 1, "green": 1, "blue": 1}
+
 
 print(sum(games))
