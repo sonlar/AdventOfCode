8f57afab9839949784f421c718b2c1aaef04fee4
sondre <z5bewil7@anonaddy.me>
2024-12-08 19:56:11 +0100

    Update create_files.py for 2024

--- create_files.py
+++ create_files.py
@@ -1,9 +1,9 @@
 import os
 
-year = "2023"
+year = "2024"
 
 os.mkdir(year)
 dir = os.path.join(year + "/", "day")
 for x in range(1, 26):
-    with open(f"{dir}{x:02d}.py", "w", encoding="utf-8") as f:
+    with open(f"{dir}{x:02d}.go", "w", encoding="utf-8") as f:
         pass
