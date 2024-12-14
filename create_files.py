0171ece7c6ed7325ba7fa6b6396ba3aa1f4e4d88
sondre <z5bewil7@anonaddy.me>
2024-12-14 21:28:36 +0100

    Add folders for each day

--- create_files.py
+++ create_files.py
@@ -3,7 +3,12 @@ import os
 year = "2024"
 
 os.mkdir(year)
-dir = os.path.join(year + "/", "day")
+dir = os.path.join(os.getcwd(), year, "day")
 for x in range(1, 26):
-    with open(f"{dir}{x:02d}.go", "w", encoding="utf-8") as f:
+    dirx = os.path.join(dir + f"{x:02d}")
+    os.mkdir(dirx)
+    os.chdir(dirx)
+    with open(f"day{x:02d}.go", "w", encoding="utf-8") as f:
         pass
+    
+    os.chdir("..")
