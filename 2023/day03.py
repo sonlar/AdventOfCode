d285827986d8506b8b0c3f42203f7bd311e132de
sonlar <31799749+sonlar@users.noreply.github.com>
2025-03-15 22:42:49 +0100

    Complete 2nd star

--- 2023/day03.py
+++ 2023/day03.py
@@ -11,27 +11,44 @@ class Day03:
         
     def find_num(self) -> None:
         num = list()
-        tempnum = str()
         y_length = len(self.f)
         x_length = len(self.f[0])
         for y,line in enumerate(self.f):
             for x, char in enumerate(line):
-                if self.is_num(char):
-                    tempnum += char
-                elif tempnum:
-                    if self.check_adjacent(y, x, len(tempnum), y_length, x_length):
-                        num.append(int(tempnum))
-                    tempnum = str()
+                if self.is_symbol(char):
+                    num.append(self.check_adjacent(y, x, y_length, x_length))
         print(sum(num))
 
-    def check_adjacent(self, y, x, num_length, y_length, x_length) -> bool:
+    def check_adjacent(self, y, x, y_length, x_length) -> int:
+        num_count = 0
+        num = list()
         for ypos in range(y-1, y+2):
             if 0 <= ypos < y_length:
-                for xpos in range(x-num_length-1,x+1):
+                num_pos = [-1]*3
+                for xpos in range(x-1,x+2):
                     if 0<= xpos < x_length:
-                        if self.is_symbol(self.f[ypos][xpos]):
-                            return True
-        return False
+                        if self.is_num(self.f[ypos][xpos]) and xpos not in range(num_pos[1], num_pos[2]+1):
+                            num_pos = self.extract_number(ypos, xpos, x_length)
+                            num.append(num_pos[0])
+                            num_count += 1
+        if num_count == 2:
+            return int(num[0]) * int(num[1])
+        return 0
+
+    def extract_number(self, y, x, x_length):
+        n = x
+        num = [str(), int(), int()]
+        while 0 <= n and self.is_num(self.f[y][n]):
+            num[0] = self.f[y][n] + num[0]
+            n -= 1
+        num[1] = n + 1
+        n = x + 1
+        while n < x_length and self.is_num(self.f[y][n]):
+            num[0] += self.f[y][n]
+            n += 1
+        num[2] = n - 1
+        return num
+        
 
     def is_symbol(self, char) -> bool:
         if match(r"[*]", char):
