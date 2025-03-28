f11eecc4008a2b0736a7365d78c86e6bc01cb3d6
sonlar <31799749+sonlar@users.noreply.github.com>
2025-03-28 13:39:38 +0100

    Implement find_location method which follows a seed through the mapping

--- 2023/day05.py
+++ 2023/day05.py
@@ -2,9 +2,9 @@ class Day05:
     def __init__(self, input) -> None:
         self.input = input
         self.seeds = list()
+        self.maps = {}
 
     def extract_mapping(self) -> None:
-        maps = {}
         current_map = None
         with open(self.input, "r") as f:
             for line in f.readlines():
@@ -13,16 +13,21 @@ class Day05:
                     self.seeds.extend(int(seed) for seed in line_split[1:])
                 elif "map" in line:
                     current_map = line_split[0]
-                    maps.update({current_map: dict()})
-                elif current_map and line_split[0].isdigit():
+                    self.maps.update({current_map: dict()})
+                elif len(line_split) > 0 and line_split[0].isdigit():
                     for i in range(int(line_split[2])):
-                        maps[current_map][int(line_split[0])+i] = maps[current_map].get(int(line_split[0])+i, int(line_split[1])+i)
-        print(maps)
+                        self.maps[current_map][int(line_split[1])+i] = self.maps[current_map].get(int(line_split[1])+i, int(line_split[0])+i)
 
-
+    def find_location(self) -> None:
+        for seed in self.seeds:
+            location = seed
+            for map in self.maps:
+                location = self.maps[map].get(location, location)
+            print(f"seed: {seed}, location: {location}")
 
 
 
 if "__main__" == __name__:
     seeds = Day05("input.txt")
     seeds.extract_mapping()
+    seeds.find_location()
