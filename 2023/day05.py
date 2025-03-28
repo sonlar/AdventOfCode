cbc38c9b265f910aaf7be74427ce12a025ef138d
sonlar <31799749+sonlar@users.noreply.github.com>
2025-03-28 13:03:07 +0100

    Extract mapping from input file

--- 2023/day05.py
+++ 2023/day05.py
@@ -1,9 +1,28 @@
-with open("input.txt", "r") as f:
-    seeds_to_plant = list()
-    seeds_map = dict()
-    for line in f.readlines():
-        if line.startswith("seeds:"):
-            seeds_to_plant.extend(int(seed) for seed in line.split()[1:])
-    print(seeds_to_plant)
+class Day05:
+    def __init__(self, input) -> None:
+        self.input = input
+        self.seeds = list()
+
+    def extract_mapping(self) -> None:
+        maps = {}
+        current_map = None
+        with open(self.input, "r") as f:
+            for line in f.readlines():
+                line_split = line.split()
+                if line.startswith("seeds:"):
+                    self.seeds.extend(int(seed) for seed in line_split[1:])
+                elif "map" in line:
+                    current_map = line_split[0]
+                    maps.update({current_map: dict()})
+                elif current_map and line_split[0].isdigit():
+                    for i in range(int(line_split[2])):
+                        maps[current_map][int(line_split[0])+i] = maps[current_map].get(int(line_split[0])+i, int(line_split[1])+i)
+        print(maps)
+
+
+
+
+
 if "__main__" == __name__:
-    pass
+    seeds = Day05("input.txt")
+    seeds.extract_mapping()
