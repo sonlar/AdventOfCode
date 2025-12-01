f542f672d177a337264a1db238958c1564dbf23f
sonlar <31799749+sonlar@users.noreply.github.com>
2025-12-01 15:17:48 +0100

    Complete day01 part two

--- 2025/day01.js
+++ 2025/day01.js
@@ -5,19 +5,23 @@ const instructions = data.split(/\s/).filter(Boolean);
 let dial = 50;
 
 const left = (instruction) => {
+  let score = 0;
   for (let i = 0; i < instruction; i++) {
     dial--;
     if (dial === -1) dial = 99;
+    if (dial === 0) score++;
   }
-  return dial ? 0 : 1;
+  return score;
 };
 
 const right = (instruction) => {
+  let score = 0;
   for (let i = 0; i < instruction; i++) {
     dial++;
     if (dial === 100) dial = 0;
+    if (dial === 0) score++;
   }
-  return dial ? 0 : 1;
+  return score;
 };
 
 const result = instructions
